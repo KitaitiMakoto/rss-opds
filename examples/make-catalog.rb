659d6b00341436417d4d4bdde8ce4cbc0c3d19b9
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-10-08 15:24:48 +0900

    Use OPDS::CATALOG_RELATIONS

--- examples/make-catalog.rb
+++ examples/make-catalog.rb
@@ -22,7 +22,7 @@ def root
     end
     maker.channel.links.new_link do |link|
       link.href = 'http://example.net/root.opds'
-      link.rel = 'start'
+      link.rel = RSS::OPDS::CATALOG_RELATIONS['start']
       link.type = RSS::OPDS::TYPES['navigation']
     end
     maker.channel.updated = '2012-08-14T04:23:00'
@@ -31,6 +31,7 @@ def root
     maker.items.new_item do |entry|
       entry.links.new_link do |link|
         link.href = 'http://example.net/popular.opds'
+        link.rel = RSS::OPDS::CATALOG_RELATIONS['popular']
         link.type = RSS::OPDS::TYPES['acquisition']
       end
       entry.title = 'Example Popular Books'
@@ -40,6 +41,7 @@ def root
     maker.items.new_item do |entry|
       entry.links.new_link do |link|
         link.href = 'http://example.net/new.opds'
+        link.rel = RSS::OPDS::CATALOG_RELATIONS['new']
         link.type = RSS::OPDS::TYPES['acquisition']
       end
       entry.title = 'Example New Books'
@@ -61,7 +63,7 @@ def popular
     end
     maker.channel.links.new_link do |link|
       link.href = 'http://example.net/root.opds'
-      link.rel = 'start'
+      link.rel = RSS::OPDS::CATALOG_RELATIONS['start']
       link.type = RSS::OPDS::TYPES['navigation']
     end
     maker.channel.updated = '2012-07-31T00:00:00'
@@ -90,7 +92,7 @@ def recent
     end
     maker.channel.links.new_link do |link|
       link.href = 'http://example.net/root.opds'
-      link.rel = 'start'
+      link.rel = RSS::OPDS::CATALOG_RELATIONS['start']
       link.type = RSS::OPDS::TYPES['navigation']
     end
     maker.channel.updated = '2012-08-14T04:23:00'
